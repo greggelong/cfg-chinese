b6dfba5e710288694555c85189b01ca8e1940157
Greg Kreisman <gkreisman_cep@qq.com>
2021-07-28 18:37:02 +0800

    update link

--- sketch.js
+++ sketch.js
@@ -35,6 +35,8 @@ function setup() {
   myp = createP("Click generate for a randomly generated Chinese Sentence")
   button = createButton('generate');
   button.mousePressed(cfg);
+  createP();
+  createP(); // put some space between button and link
   mylink = createA('https://github.com/greggelong/cfg-chinese', 'link to this  repo');
 }
 
